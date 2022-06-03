98232ae5731b16ad2ff99f1d20a2d657aba8fae1
Karen Jully <91673156+Kjully01@users.noreply.github.com>
2022-06-03 15:31:14 -0300

    navegação entre as telas

--- app/src/main/java/br/com/login/LoginFragment.kt
+++ app/src/main/java/br/com/login/LoginFragment.kt
@@ -5,14 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import br.com.login.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        listener()
+    }
+
+    private fun listener(){
+        binding.btnLogin.setOnClickListener{
+            findNavController().navigate(
+                LoginFragmentDirections.actionLoginFragmentToHomeFragment()
+            )
+        }
+
+        binding.tvSingUp.setOnClickListener{
+            findNavController().navigate(
+                LoginFragmentDirections.actionLoginFragmentToRegistrationFragment()
+            )
+        }
     }
 }
--- app/src/main/java/br/com/login/MenuFragment.kt
+++ app/src/main/java/br/com/login/MenuFragment.kt
@@ -5,14 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import br.com.login.databinding.FragmentMenuBinding
 
 class MenuFragment : Fragment() {
+
+    private lateinit var binding: FragmentMenuBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+    ): View {
+        binding = FragmentMenuBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        listener()
+    }
+
+    private fun listener(){
+        binding.btnLogin.setOnClickListener{
+            findNavController().navigate(
+                MenuFragmentDirections.actionMenuFragmentToLoginFragment()
+            )
+        }
+
+        binding.btnSingUp.setOnClickListener{
+            findNavController().navigate(
+                MenuFragmentDirections.actionMenuFragmentToRegistrationFragment()
+            )
+        }
     }
 }
